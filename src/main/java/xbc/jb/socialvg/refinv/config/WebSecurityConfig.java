ac96899c41b09bf71a072e61be33581735ee03d2
Abdelfettah BESBES <abbesbes@e1r12p1.unit.ua>
2019-09-20 16:30:39 +0300

    configuring web security

---
+++ src/main/java/xbc/jb/socialvg/refinv/config/WebSecurityConfig.java
@@ -0,0 +1,75 @@
+package xbc.jb.socialvg.refinv.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import xbc.jb.socialvg.refinv.service.UserServiceDb;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserServiceDb userServiceDb;
+
+    @Bean
+    PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    DaoAuthenticationProvider daoAuthenticationProvider()
+    {
+        DaoAuthenticationProvider daoAuthenticationProvider
+                = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userServiceDb);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    SimpleUrlAuthenticationSuccessHandler successHandler()
+    {
+        SimpleUrlAuthenticationSuccessHandler successHandler
+                = new SimpleUrlAuthenticationSuccessHandler();
+        successHandler.setDefaultTargetUrl("/dashboard");
+        return successHandler;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //super.configure(auth);
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+        http
+                .authorizeRequests()
+                .antMatchers("/", "register").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .successHandler(successHandler())
+                //.defaultSuccessUrl("/dashboard", true)
+                ;
+    }
+}
