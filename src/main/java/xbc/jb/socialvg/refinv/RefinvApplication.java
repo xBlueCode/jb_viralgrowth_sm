4189ba5949e584ffdd187062b6df3db1813393a8
Abdelfettah BESBES <abbesbes@e1r12p1.unit.ua>
2019-09-20 15:34:47 +0300

    Preparing project's structure

---
+++ src/main/java/xbc/jb/socialvg/refinv/RefinvApplication.java
@@ -0,0 +1,13 @@
+package xbc.jb.socialvg.refinv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefinvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefinvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xbc/jb/socialvg/refinv/RefinvApplicationTests.java
@@ -0,0 +1,16 @@
+package xbc.jb.socialvg.refinv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RefinvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
